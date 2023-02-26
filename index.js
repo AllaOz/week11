d4891730d89552614dfb22db2377806bbfbcc1b9
AllaOz <ozcanalla@gmail.com>
2023-02-26 18:26:06 +0300

    added index.js

---
+++ index.js
@@ -0,0 +1,23 @@
+let item1 = document.querySelector('.sneakers__price');
+console.log (item1)
+let item1Value = +item1.innerHTML;
+console.log(item1Value); 
+
+let item2 = document.querySelector('.ballet__price');
+console.log (item2)
+let item2Value = +item2.innerHTML;
+console.log(item2Value);
+
+let item3 = document.querySelector('.heels__price');
+console.log (item3)
+let item3Value = +item3.innerHTML;
+console.log(item3Value);
+
+let item4 = document.querySelector('.boots__price');
+console.log (item3)
+let item4Value = +item3.innerHTML;
+console.log(item4Value);
+
+
+
+
