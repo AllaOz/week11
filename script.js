8161b6553cd09ae781c5bb445934c18a2f5f8fdf
AllaOz <ozcanalla@gmail.com>
2023-02-26 21:33:58 +0300

    script.js added

---
+++ script.js
@@ -0,0 +1,44 @@
+function applyDiscount(price) {
+	return price * 0.8; 
+  }
+  let item1 = document.querySelector('.price__sneakers');
+  let item1Value = +item1.innerHTML;
+  let discountedItem1Value = applyDiscount(item1Value);
+  console.log(discountedItem1Value);
+  
+  let item2 = document.querySelector('.ballet__price');
+  let item2Value = +item2.innerHTML;
+  let discountedItem2Value = applyDiscount(item2Value);
+  console.log(discountedItem2Value);
+  
+  let item3 = document.querySelector('.heels__price');
+  let item3Value = +item3.innerHTML;
+  let discountedItem3Value = applyDiscount(item3Value);
+  console.log(discountedItem3Value);
+  
+  let item4 = document.querySelector('.boots__price');
+  let item4Value = +item4.innerHTML;
+  let discountedItem4Value = applyDiscount(item4Value);
+  console.log(discountedItem4Value);
+  
+  let arr = [discountedItem1Value, discountedItem2Value, discountedItem3Value, discountedItem4Value];
+  
+  let result = arr.reduce(function(sum, elem) {
+	  return sum + elem;
+  }, 0);
+  console.log(result);
+  function applyCoupon(discount) {
+	let cartTotal = document.getElementById('result');
+	let totalValue = result - (result * discount / 100);
+	cartTotal.innerHTML = totalValue.toFixed(2);
+  }
+  
+  let resultElement = document.getElementById("result");
+  resultElement.innerHTML = result.toFixed(2);
+  
+  let couponButton = document.querySelector('button');
+  couponButton.onclick = function() {
+	applyCoupon(20);
+  }
+ item1Value.textContent = `${discountedItem1Value} ${item1Value[1]}`;
+
